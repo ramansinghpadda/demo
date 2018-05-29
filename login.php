0436cf2f88c99e8206a6770a13a9bbfdf9567ab8
root <mailtorsapadd@gmail.com>
2018-05-30 00:23:29 +0530

    add composer.json for PHP app detection

--- login.php
+++ login.php
@@ -3,12 +3,10 @@ require_once dirname(__FILE__).'/config.php';
 require_once dirname(__FILE__).'/ApiManager.php';
 
 
-$data = file_get_contents('php://input');
 
-if(ApiManager::validateJSON($data)){
-	$data = ApiManager::parseJSON($data);
-	(new ApiManager)->login($data);
+if(isset($_POST['username'])){
+	(new ApiManager)->login($_POST);
 }else{
 	@http_response_code(400);
-	echo json_encode(['message'=>'Invalid json request'],JSON_PRETTY_PRINT);
+	echo json_encode(['message'=>'Invalid data'],JSON_PRETTY_PRINT);
 }
